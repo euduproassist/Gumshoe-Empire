5c4d54aea8005e2ef5dc8aa9e410356a0b5da7bc
eduproassist <eduproassist44@gmail.com>
2026-07-20 12:20:26 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,8 @@
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-storage.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-analytics.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -11,5 +15,15 @@ const firebaseConfig = {
   appId: "1:270581086181:web:102b85ed238ec2936a210c",
   measurementId: "G-HCN6L178TV"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Services to be exported
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
+
 
 
