e51243e2b09b3d357adff32a0f5c7b5519bb2e2f
eduproassist <eduproassist44@gmail.com>
2026-07-20 12:17:46 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,15 @@
 
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBJHPb-CIJ64dipaT7xkE8CiZA0KTCvQho",
+  authDomain: "gumshoe-empire.firebaseapp.com",
+  projectId: "gumshoe-empire",
+  storageBucket: "gumshoe-empire.firebasestorage.app",
+  messagingSenderId: "270581086181",
+  appId: "1:270581086181:web:102b85ed238ec2936a210c",
+  measurementId: "G-HCN6L178TV"
+};
+
+
